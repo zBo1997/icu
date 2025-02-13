178540e344fcc65a6d638d00f5dca6787d921d4a
MoMo <zhubo@vv.cn>
2025-02-13 15:01:47 +0800

    sql modify

--- scripts/table.sql
+++ scripts/table.sql
@@ -1,9 +1,12 @@
 -- Active: 1738724774868@@172.16.6.69@3306@icu
-CREATE TABLE icu.users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
-    name VARCHAR(255) COMMENT 'Name',
-    email VARCHAR(255) COMMENT 'Email',
-    username VARCHAR(255) COMMENT 'Username',
-    password VARCHAR(255) COMMENT 'Password',
-    create_time DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Create Time'
-) COMMENT 'user';
+-- auto-generated definition
+create table icu.users (
+    id int auto_increment comment 'Primary Key' primary key,
+    name varchar(255) null comment 'Name',
+    avatar varchar(255) null comment 'avatar',
+    email varchar(255) null comment 'Email',
+    username varchar(255) null comment 'Username',
+    signature varchar(255) null comment 'signature',
+    password varchar(255) null comment 'Password',
+    create_time datetime default CURRENT_TIMESTAMP null comment 'Create Time'
+) comment 'user';
